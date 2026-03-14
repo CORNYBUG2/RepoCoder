cdcbb98f85542f1c04271ba9b41b461c1485a5b6
CORNYBUG2 <viditkumar2005@gmail.com>
2026-03-14 21:17:40 +0530

    Changing generative model to deepseek-coder from salesforce-codegen

--- generator/llm_generator.py
+++ generator/llm_generator.py
@@ -4,30 +4,27 @@ import torch
 
 class LLMGenerator:
 
-    def __init__(self, model_name="Salesforce/codegen-350M-mono"):
+    def __init__(self, model_name="deepseek-ai/deepseek-coder-1.3b-base"):
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-
         print("Using device:", self.device)
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
         self.model = AutoModelForCausalLM.from_pretrained(
             model_name,
-            torch_dtype=torch.float16 if self.device == "cuda" else torch.float32
+            torch_dtype=torch.float16,
+            device_map="auto"
         )
-
-        self.model.to(self.device)
 
     def generate(self, prompt, max_tokens=120):
 
-        inputs = self.tokenizer(prompt, return_tensors="pt").to(self.device)
+        inputs = self.tokenizer(prompt, return_tensors="pt").to(self.model.device)
 
         outputs = self.model.generate(
             **inputs,
             max_new_tokens=max_tokens,
-            temperature=0.7,
-            do_sample=True
+            do_sample=False
         )
 
         return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
